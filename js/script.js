974957567fef0b416e81cce73b452192612c0dfc
FedericoPiazzolla <Federicop257@gmail.com>
2023-10-19 17:49:37 +0200

    add: calculated discount

--- js/script.js
+++ js/script.js
@@ -9,9 +9,35 @@ console.log(userAgeElem);
 
 const sendBtn = document.getElementById("send");
 sendBtn.addEventListener("click", function(){
+
   // Raccolta dati
   const userName = userNameElem.value;
   const userKm = userKmElem.value;
   const userAge = userAgeElem.value;
   console.log(userName, userKm, userAge);
+
+  // Calcolo scontistiche
+  const ticketPriceKm = 0.21;
+  const ticketPriceTotal = ticketPriceKm * userKm;
+  console.log(ticketPriceTotal.toFixed(2))
+
+  let discount = "";
+  let toalPrice = "";
+
+  if (userAge === "over-65") {
+    discount = (ticketPriceTotal / 100) * 40;
+    console.log(discount);
+    totalPrice = (ticketPriceTotal - discount);
+    console.log(totalPrice);
+  } else if (userAge === "minorenne") {
+    discount = (ticketPriceTotal / 100) * 20;
+    console.log(discount);
+    totalPrice = (ticketPriceTotal - discount);
+    console.log(totalPrice);
+  } else {
+    discount = 0
+    totalPrice = (ticketPriceTotal - discount);
+    console.log(totalPrice);
+  }
+  
 })
